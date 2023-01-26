3f005f0b9a681b8a9ed4569168c7dd7fca5bac50
cyx <vsuh@mail.ru>
2023-01-26 17:25:46 +0300

    добавил комментарии

--- backup_flash.cmd
+++ backup_flash.cmd
@@ -1,12 +1,16 @@
 @echo off
+call :set_dateParts
 call :read_settings
 call :check_usb_flash
 call :do_backup
 call :delete_old_arcs
-echo Finished(%ERRORLEVEL%)
+
 exit
 
-
+:: VSCraft(c)2023 
+:: Резервное копирование содержимого USB flash drive
+:: Скрипт должен выполняться, когда флешка с файлом seal.seal устанавливается в порт
+::###############################[ SUBROUTINES ]##################################::
 :check_usb_flash
 if NOT exist %SEAL% (@echo SEAL file %SEAL% not found
 	exit
@@ -21,17 +25,23 @@ if NOT exist %RAR% (@echo RAR executable not found
 exit /b
 
 :read_settings
-set bk_path=d:\backups\USBFLASH
-set sr_path=d:\IRIS
+set BACKUP_PATH=d:\backups\USBFLASH
+set error_log=d:\backups\log\%yy%-%mm%-%dd%_error.log
+set ФЛЕШ=d:\IRIS
 set SEAL=d:\IRIS\seal.seal
 set RAR=d:\iris\bin\rar.exe
 set curr_path=%CD%
-set f2k=5
+set СохранитьФайлов=5
+set pw=qwerf
 exit /b
 
 :do_backup
-cd /d %sr_path%
-%RAR% a -r -agYYYY-MM-DD -xbin -xseal.seal -x"System Volume Information" -y -- %bk_path%\IRIS_ *.*
+cd /d %ФЛЕШ%
+%RAR% a -r -u -p%pw% -agYYYY-MM-DD ^
+		-es ^
+		-y -ilog%error_log% ^
+		%BACKUP_PATH%\IRIS_ *.*
+
 cd /d %curr_path%
 
 exit /b
@@ -39,8 +49,14 @@ exit /b
 :delete_old_arcs
 SETLOCAL ENABLEDELAYEDEXPANSION
 Set ii=0
-echo DELETE all except %f2k% files from "%bk_path%" directory
-for /f  %%I in ('dir /o:-d /a:-d /b %bk_path%') DO (
+echo DELETE all except %СохранитьФайлов% files from "%BACKUP_PATH%" directory
+for /f  %%I in ('dir /o:-d /a:-d /b %BACKUP_PATH%') DO (
 	Set /a ii=ii+1
-	if !ii! GTR %f2k% echo !ii!: DELETE %%I && del %bk_path%\%%I
+	if !ii! GTR %СохранитьФайлов% echo !ii!: DELETE %%I && del %BACKUP_PATH%\%%I
 )
+
+:set_dateParts
+
+Set yy=%date:~6,4%
+Set mm=%date:~3,2%
+Set dd=%date:~0,2%
