d750d68e2391218e1c18c6834a398d5a0d9b4152
cyx <vsuh@mail.ru>
2023-01-28 14:17:08 +0300

    Запустить задачу после создания (если еще не запущена)

--- set_sheduler.cmd
+++ set_sheduler.cmd
@@ -1,21 +1,30 @@
 :: Регистрирует задачу в планировщике заданий по запуску скрипта register_USB_insert.cmd при старте компьютера
 cd /d %~dp0
+Set cmd.log=Scheduler_Register.LOG
+
 if `%1`==`admin` goto iamadmin
 reg.exe query "HKU\S-1-5-19">nul 2>&1
 if %errorlevel% equ 1 goto UACPrompt
 
 :iamadmin
-Set task.name="\cyx\USB_EVENT_REGISTER"
-Set task.cmd="%~dp0\register_USB_insert.cmd admin"
-
-
-SCHTASKS /Create /RU "NT AUTHORITY\LOCALSERVICE" /SC ONSTART ^
- /TN %task.name% /TR %task.cmd% /F /Z
+Set task.name=\cyx\USB_EVENT_REGISTER
+Set task.exe=USBPlugEvent
+Set task.cmd="%~dp0register_USB_insert.cmd admin"
+>>%cmd.log% %date% %time% ==============================================
+>>%cmd.log%      echo %time% ^> SCHTASKS /Create /RU SYSTEM /SC ONSTART /TN "%task.name%" /TR %task.cmd% /F
+>>%cmd.log% 2>&1 SCHTASKS /Create /RU SYSTEM /SC ONSTART /TN "%task.name%" /TR %task.cmd% /F
 
 echo err=%ERRORLEVEL%
-
+tasklist /FI "IMAGENAME eq %task.exe%*"|find /I "%task.exe%" && ( 
+	echo %time% Программа %task.exe% уже выполняется>>%cmd.log% 
+	) || (
+	>>%cmd.log%      echo %time% ^> SCHTASKS /Run /I /TN %task.name%
+	>>%cmd.log% 2>&1 SCHTASKS /Run /I /TN "%task.name%"
+) 
 exit
 
 :UACPrompt
 mshta "vbscript:CreateObject("Shell.Application").ShellExecute("%~fs0", "", "", "runas", 1) & Close()"
+type %cmd.log%
+
 exit /b
