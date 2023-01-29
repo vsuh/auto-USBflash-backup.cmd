54fe306119f1a03e39103d1314d59909f9f48d5b
cyx <vsuh@mail.ru>
2023-01-29 13:53:23 +0300

    Исправил наименование задачи

--- save_to_remotes.cmd
+++ save_to_remotes.cmd
@@ -3,9 +3,11 @@
 ::# backup.dest.ssh.2 = helor:BACKUPS/IRIS
 ::# backup.files.path = d:\backups\USBFLASH
 ::# backup.keypath = C:\Users\vsuh\.ssh\id_rsa
+::
 SETLOCAL ENABLEDELAYEDEXPANSION
-@echo on
+@echo off
 Set me=%0
+Set log=%~dpn0.log
 Set _cd=%CD%
 call :configure
 cd %backup.files.path%
@@ -15,6 +17,11 @@ for /f "usebackq delims== tokens=1,2" %%I in (`set backup.dest`) DO (
 	IF "!var:~12,3!"=="ssh" ( 
 	echo ^*^*^*^*^ SSH copy to !val!
 		scp -i !backup.keypath! *.rar !val! 
+		if `!ERRORLEVEL!`==`0` ( 
+			echo !date! !time! !val! SUCCESS >>!log!
+				) ELSE ( 
+			echo !date! !time! !val! ERROR {!ERRORLEVEL!} >>!log! 
+		)
 	)
 	
 
@@ -22,10 +29,11 @@ for /f "usebackq delims== tokens=1,2" %%I in (`set backup.dest`) DO (
 cd /d %_cd%
 EXIT
 :configure
-@echo off
+
 FOR /F "eol=; tokens=1,2,3,4* delims==# usebackq" %%I in (`findstr /B /C:"::#" %me%`) DO @(
     @set str=%%K
     @set var=%%J
     @SET !var: =!=!str: =!
 )
+
 exit /b
