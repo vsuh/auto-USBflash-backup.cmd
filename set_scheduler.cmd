9947161b8cd9a891e32ef1adb6fcef083c916537
cyx <vsuh@mail.ru>
2023-02-08 08:45:14 +0300

    добавил создание второй задачи запуска резервного копирования в 18:30 и при входе пользователя

--- set_scheduler.cmd
+++ set_scheduler.cmd
@@ -8,19 +8,22 @@ reg.exe query "HKU\S-1-5-19">nul 2>&1
 if %errorlevel% equ 1 goto UACPrompt
 
 :iamadmin
-SETLOCAL ENABLEDELAYEDEXPANSION
-cd /d %~dp0 && call configure
+SETLOCAL ENABLEDELAYEDEXPANSION && cd /d %~dp0 && call configure
 
 >>%cmd.log% echo %date% %time% ==============================================
->>%cmd.log% echo %time% ^> SCHTASKS /Create /RU SYSTEM /SC ONSTART /TN "%vsuh.task.name%" /TR %vsuh.task.cmd% /F
->>%cmd.log% 2>&1           SCHTASKS /Create /RU SYSTEM /SC ONSTART /TN "%vsuh.task.name%" /TR %vsuh.task.cmd% /F
+>>%cmd.log% echo %time% ^> SCHTASKS /Create /RU SYSTEM /SC ONSTART /TN "%vsuh.task.name%" /TR "%~dp0!vsuh.task.cmd!" /F
+>>%cmd.log% 2>&1           SCHTASKS /Create /RU SYSTEM /SC ONSTART /TN "%vsuh.task.name%" /TR "%~dp0!vsuh.task.cmd!" /F
+
+>>%cmd.log% echo %time% ^> SCHTASKS /Create /RU SYSTEM /SC ONLOGON /TN "%vsuh.task1.name%" /TR "!vsuh.register.cmd!" /F
+>>%cmd.log% 2>&1           SCHTASKS /Create /RU SYSTEM /SC ONLOGON /TN "%vsuh.task1.name%" /TR "!vsuh.register.cmd!" /F
 
 echo err=%ERRORLEVEL%
 tasklist /FI "IMAGENAME eq %vsuh.task.exe%*"|find /I "%vsuh.task.exe%" && ( 
-	>>%cmd.log%  echo %time% Программа %vsuh.task.exe% уже выполняется
-	) || (
-	>>%cmd.log%  echo %time% ^> SCHTASKS /Run /I /TN "%vsuh.task.name%"
-	>>%cmd.log% 2>&1            SCHTASKS /Run /I /TN "%vsuh.task.name%" ) 
+	>>%cmd.log% echo %time% Программа %vsuh.task.exe% уже выполняется. Завершаю процесс.
+	>>%cmd.log% tasklist /FI "IMAGENAME eq %vsuh.task.exe%*"
+	) 
+>>%cmd.log%  echo %time% ^> SCHTASKS /Run /I /TN "%vsuh.task.name%"
+>>%cmd.log% 2>&1            SCHTASKS /Run /I /TN "%vsuh.task.name%" ) 
 exit
 
 :UACPrompt
